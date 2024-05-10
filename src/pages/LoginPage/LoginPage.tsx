8ad404ccb155b5a14c4cfe66eb4518d480ffc5ec
Ikbal Čaušević <ikbalcaus@gmail.com>
2024-05-10 12:25:03 +0200

    Created login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
@@ -43,5 +44,23 @@ export default function LoginPage() {
         </div>
       </div>
     </div>
+=======
+import { Link } from 'react-router-dom'
+import styles from './LoginPage.module.css'
+import image from '../../assets/images/login-removebg-preview.png'
+
+export default function LoginPage() {
+  return (
+    <>
+      <img src={image} alt="login" className={styles.image} />
+      <input type="text" className={styles.input} placeholder="Email address" />
+      <input type="password" className={styles.input} placeholder="Password" />
+      <input type="button" className={styles.button} value="Login" />
+      <div className={styles.links}>
+        <Link to="/signup" className={styles.link}>Go to Sign Up</Link>
+        <Link to="/forgot-password" className={styles.link}>Forgot password?</Link>
+      </div>
+    </>
+>>>>>>> f322c5b (Created login page)
   )
 }
