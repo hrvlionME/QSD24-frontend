291c4ef650a11c3f0c5c095c493b10cf5353e363
Ikbal Čaušević <ikbalcaus@gmail.com>
2024-05-10 12:25:03 +0200

    Created login page

---
+++ src/globals.d.ts
@@ -0,0 +1,18 @@
+declare module "*.module.css";
+declare module "*.module.scss";
+declare module '*.jpg' {
+    const value: any;
+    export = value;
+}
+declare module '*.jpeg' {
+    const value: any;
+    export = value;
+}
+declare module '*.png' {
+    const value: any;
+    export = value;
+}
+declare module '*.gif' {
+    const value: any;
+    export = value;
+}
