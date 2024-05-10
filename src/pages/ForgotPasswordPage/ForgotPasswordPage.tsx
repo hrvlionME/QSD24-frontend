43f75611a00368a2f55b7a2761100c044e762d1e
hrvojekozul <hrvlionme@gmail.com>
2024-05-10 13:39:12 +0200

    forgot password GUI

--- src/pages/ForgotPasswordPage/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage/ForgotPasswordPage.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import styles from './ForgotPassowrdPage.module.css';
+import React, { useState } from 'react';
+import styles from './ForgotPasswordPage.module.css';
+import image from '../../assets/images/forgotPassword.png'
+import { FiArrowLeftCircle } from "react-icons/fi";
+import { Link } from 'react-router-dom';
 
 export default function ForgotPassowrdPage() {
+
+  const [email, setEmail] = useState("");
+
+  function handleEmailChange(event: any) {
+    setEmail(event.target.value);
+  }
+
+  function isValidEmail(email: string) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+  
+
   return (
     <>
-        <div>
-            <button></button>
-            <img src="" alt="" />
-            <input type="text" />
-            <button></button>
-        </div>
+      <div className={styles.root}>
+        <Link  to="/login" className={styles.backArrow}><FiArrowLeftCircle/></Link>
+        <img src={image} alt="" className={styles.img} />
+        <input type="text" className={styles.emailInput} placeholder="Email address" value={email} onChange={handleEmailChange} />
+        <button className={styles.btn} disabled={!isValidEmail(email)}>Send email</button>
+      </div>
     </>
   );
 }
