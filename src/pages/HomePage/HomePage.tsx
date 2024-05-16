d160fc64b2aafa8ac106eb20890ab614a41051a2
Ikbal Čaušević <ikbalcaus@gmail.com>
2024-05-16 17:26:56 +0200

    Finished last section of home page and responsivity

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -13,9 +13,7 @@ export default function HomePage() {
         <div className={styles.heroContent}>
           <div className={styles.heroSmText}>IN THE MOOD FOR NEW</div>
           <div className={styles.heroLgText}>STYLE</div>
-          <Link to="shop/all/1" className={styles.heroButton}>
-            SHOP NOW
-          </Link>
+          <Link to="shop/all/1" className={styles.heroButton}>SHOP NOW</Link>
         </div>
       </div>
       <div className={styles.categorySection}>
@@ -47,25 +45,8 @@ export default function HomePage() {
           <div className={styles.misionVisionBox}>
             <div className={styles.misionVisionTitle}>- QSD Shop -</div>
             <div className={styles.misionVisionText}>
-              <div>
-                Our mission is to provide our customers with a seamless online
-                shopping experience for high-quality, stylish clothing that
-                empowers them to look and feel their best. We are committed to
-                offering a wide range of modern clothing options that are both
-                functional and fashionable, and we aim to make our customers
-                lives easier by delivering exceptional service and exceptional
-                products at affordable prices.
-              </div>
-              <div>
-                Our vision is to become the go-to online destination for quality
-                and modern clothing that inspires confidence and individuality.
-                We strive to be a trusted source for fashion-forward individuals
-                who value style and substance in their clothing choices. By
-                continuously innovating and adapting to changing fashion trends,
-                we aim to remain at the forefront of the online fashion
-                industry, while maintaining our commitment to providing
-                excellent customer service and high-quality products.
-              </div>
+              <div>Our mission is to provide our customers with a seamless online shopping experience for high-quality, stylish clothing that empowers them to look and feel their best. We are committed to offering a wide range of modern clothing options that are both functional and fashionable, and we aim to make our customers lives easier by delivering exceptional service and exceptional products at affordable prices.</div>
+              <div>Our vision is to become the go-to online destination for quality and modern clothing that inspires confidence and individuality. We strive to be a trusted source for fashion-forward individuals who value style and substance in their clothing choices. By continuously innovating and adapting to changing fashion trends, we aim to remain at the forefront of the online fashion industry, while maintaining our commitment to providing excellent customer service and high-quality products.</div>
             </div>
           </div>
         </div>
