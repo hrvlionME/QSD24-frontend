163310be63742e20e1459aca767744baead4fc4f
Hrvoje Kožul <hrvlionME@gmail.com>
2024-05-17 09:52:16 +0200

    qsd-46 rebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
   BrowserRouter,
   Routes,
@@ -8,42 +8,43 @@ import {
   Outlet,
 } from "react-router-dom";
 import NavbarPage from "./components/Navbar/Navbar";
-import HomePage from './pages/HomePage/HomePage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import SignUpPage from './pages/SignupPage/SignupPage';
-import ForgotPasswordPage from './pages/ForgotPasswordPage/ForgotPasswordPage';
+import HomePage from "./pages/HomePage/HomePage";
+import ShopPage from "./pages/ShopPage/ShopPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import SignUpPage from "./pages/SignupPage/SignupPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage/ForgotPasswordPage";
 import './index.css';
 import './colors.css'
 import Faq from './pages/FAQ/Faq';
 import ContactUs from './pages/ContactUsPage/ContactUs';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
-import TwoFA from './pages/TwoFA/TwoFA';
 import UPFavoritesPage from './pages/UserPanel/UPFavoritesPage/UPFavoritesPage';
 import UPUserDataPage from './pages/UserPanel/UPUserDataPage/UPUserDataPage';
+import TwoFA from './pages/TwoFA/TwoFA';
+
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />}>
-            <Route path="/" element={ <HomePage /> } />
-            <Route path="/login" element={ <LoginPage /> } />
-            <Route path="/signup" element={ <SignUpPage /> } />
-            <Route path="/forgot-password" element={ <ForgotPasswordPage /> } />
-            <Route path="/sendCode" element={ <TwoFA /> } />
-            <Route path="/contact-us" element={ <ContactUs /> }></Route>
-            <Route path="/faq" element={ <Faq /> } />
-            <Route path="/shop/favorites/:id" element={ <FavoritesPage /> } />
-            <Route path="/profile" element={ <UPUserDataPage /> } />
-            <Route path="/profile/favorites" element={ <UPFavoritesPage /> } />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  )
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/sendCode" element={ <TwoFA /> } />
+          <Route path="/contact-us" element={ <ContactUs /> }></Route>
+          <Route path="/faq" element={ <Faq /> } />
+          <Route path="/shop/:category/:id" element={<ShopPage />} />
+          <Route path="/profile" element={ <UPUserDataPage /> } />
+          <Route path="/profile/favorites" element={ <UPFavoritesPage /> } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+  );
 }
 
 function Main() {
