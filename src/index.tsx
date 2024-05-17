eac6df6dd58cc09790b6aa8feba38ae1520e1c9b
Hrvoje Kožul <hrvlionME@gmail.com>
2024-05-17 09:52:16 +0200

    qsd-46 rebase

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,10 @@ import LoginPage from './pages/LoginPage/LoginPage';
 import SignUpPage from './pages/SignupPage/SignupPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage/ForgotPasswordPage';
 import './colors.css';
-
-
 import './index.css';
 import './colors.css'
 import Faq from './pages/FAQ/Faq';
+import ContactUs from './pages/ContactUsPage/ContactUs';
 
 function App() {
   return (
@@ -20,6 +19,7 @@ function App() {
         <Route path="/login" element={ <LoginPage /> } />
         <Route path="/signup" element={ <SignUpPage /> } />
         <Route path="/forgot-password" element={ <ForgotPasswordPage /> } />
+        <Route path="/contact-us" element={ <ContactUs /> }></Route>
         <Route path="/faq" element={ <Faq /> } />
       </Routes>
     </BrowserRouter>
