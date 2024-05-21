1195e5dc28914377844f39a8048416e5dcbdf60e
Hrvoje Kožul <hrvlionME@gmail.com>
2024-05-21 09:37:10 +0200

    rebase

--- src/pages/HomePage/NewInThisWeek/NewInThisWeek.tsx
+++ src/pages/HomePage/NewInThisWeek/NewInThisWeek.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import styles from './NewInThisWeek.module.css';
 import { FaRegHeart } from "react-icons/fa";
@@ -39,27 +38,6 @@ export default function NewInThisWeek() {
                             <button className={styles.productPrice}>{card.price}</button>
                             <div className={styles.productName}>{card.name}</div>
                             <div className={styles.productDescription}>{card.description}</div>
-=======
-import styles from './NewInThisWeek.module.css'
-import { FaRegHeart } from "react-icons/fa";
-
-export default function NewInThisWeek() {
-    return (
-        <>
-            <div className={styles.title}>• NEW IN THIS WEEK •</div>
-            <div className={styles.cardsContainer}>
-                {[1,2,3,4].map(() => (
-                    <div className={styles.card}>
-                        <div className={styles.cardImageWrapper}>
-                            <img src="https://picsum.photos/200/300" className={styles.cardImage} alt="product" />
-                            <button className={styles.buttonOnImage}>BUY</button>
-                        </div>
-                        <div className={styles.cardContent}>
-                            <button className={styles.productPrice}>100$</button>
-                            <div className={styles.productName}>Product name</div>
-                            <div className={styles.productDescription}>Product description</div>
-                            <div className={styles.productDescription}>Product description</div>
->>>>>>> 8bbe104 (Added "new in this week" section)
                             <div className={styles.cardFooter}>
                                 <FaRegHeart style={{ cursor: "pointer" }} />
                                 <div style={{ cursor: "pointer" }}>BUY</div>
@@ -68,15 +46,11 @@ export default function NewInThisWeek() {
                     </div>
                 ))}
             </div>
-<<<<<<< HEAD
             <div className={styles.dots}>
                 {[...Array(totalDots)].map((_, index) => (
                     <div key={index} className={`${styles.dot} ${(index === activeDot) && styles.activeDot}`} onClick={() => setActiveDot(index)}></div>
                 ))}
             </div>
         </div>
-=======
-        </>
->>>>>>> 8bbe104 (Added "new in this week" section)
     )
 }
