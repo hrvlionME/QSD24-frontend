777467812a807fa9a561fe5a9c48811c6fa98991
Hrvoje Kožul <hrvlionME@gmail.com>
2024-05-21 10:18:57 +0200

    remove div {}

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
 import TwoFA from './pages/TwoFA/TwoFA';
+import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
 
 function App() {
   return (
