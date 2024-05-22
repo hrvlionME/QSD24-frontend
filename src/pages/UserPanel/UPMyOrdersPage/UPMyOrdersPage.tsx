e69d7261ae888e66c7c7f54d4e14c0384b70a45a
Ikbal Čaušević <ikbalcaus@gmail.com>
2024-05-22 16:14:01 +0200

    Added my orders page

--- src/pages/UserPanel/UPMyOrdersPage/UPMyOrdersPage.tsx
+++ src/pages/UserPanel/UPMyOrdersPage/UPMyOrdersPage.tsx
@@ -1,16 +1,17 @@
 import UPNav from "../UPNav/UPNav"
 import UPTitle from "../UPTitle/UPTitle"
-import styles from "./UPFavoritesPage.module.css"
+import styles from "./UPMyOrdersPage.module.css"
 import Footer from "../../../components/Footer/Footer"
 
-export default function UPFavoritesPage() {
+export default function UPMyOrdersPage() {
   return (
     <div style={{ backgroundColor: "var(--primary-color-2)" }}>
       <UPTitle />
       <div className={styles.page}>
-        <UPNav active="5" />
+        <UPNav active="4" />
         <div className={styles.content}>
-          <div style={{ fontWeight: "600" }}>You don't have any products in your favorites.</div>
+          <div style={{ fontWeight: "600" }}>You don't have any orders yet.</div>
+          <button className={styles.button}>1</button>
         </div>
       </div>
       <Footer />
