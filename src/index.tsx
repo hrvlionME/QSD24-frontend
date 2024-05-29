8de02372bf9938732901b3077660dfa5d682ab08
Hrvoje Kožul <hrvlionME@gmail.com>
2024-05-29 10:50:44 +0200

    minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -48,13 +48,13 @@ function App() {
             <Route path="/shop/favorites/:id" element={<FavoritesPage />} />
             <Route path="/profile" element={<UPUserDataPage />} />
             <Route path="/profile/edit" element={<UPEditProfilePage />} />
-            <Route path="/product/:id" element={<ProductDetailsPage />} />
             <Route
               path="/profile/change-password"
               element={<UPChangePasswordPage />}
             />
             <Route path="/profile/my-orders" element={<UPMyOrdersPage />} />
             <Route path="/profile/favorites" element={<UPFavoritesPage />} />
+            <Route path="/product/:id" element={<ProductDetailsPage />} />
             <Route path="/admin/:category" element={<AdminPage />} />
           </Route>
         </Routes>
