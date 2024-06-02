b5f45a2ebc3eb1a1966a16185b5f0fe793753b89
Ikbal Čaušević <ikbalcaus@gmail.com>
2024-06-02 21:23:20 +0200

    Added admin sizes page

--- src/services/sizes.ts
+++ src/services/sizes.ts
@@ -11,3 +11,39 @@ export const getSizes = async () => {
         else throw new Error("Failed to get data");
     }
 }
+
+export const addSize = async (req: any) => {
+    try {
+        const response = await axiosClient.post("/addSize", req);
+        return response.data;
+    }
+    catch (error: any) {
+        if (error.response) throw new Error(error.response.data.message);
+        else if (error.request) throw new Error("No response received from server");
+        else throw new Error("Failed to get data");
+    }
+}
+
+export const editSize = async (req: any) => {
+    try {
+        const response = await axiosClient.put("/updateSize", req);
+        return response.data;
+    }
+    catch (error: any) {
+        if (error.response) throw new Error(error.response.data.message);
+        else if (error.request) throw new Error("No response received from server");
+        else throw new Error("Failed to get data");
+    }
+}
+
+export const deleteSize = async (id: any) => {
+    try {
+        const response = await axiosClient.delete(`/deleteSize/${id}`);
+        return response.data;
+    }
+    catch (error: any) {
+        if (error.response) throw new Error(error.response.data.message);
+        else if (error.request) throw new Error("No response received from server");
+        else throw new Error("Failed to get data");
+    }
+}
