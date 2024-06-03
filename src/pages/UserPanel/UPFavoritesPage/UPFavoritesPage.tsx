ef13a009a8fcaf75143f73fda114be9491633b90
Hrvoje Kožul <hrvlionME@gmail.com>
2024-06-03 13:06:47 +0200

    UP favorite fetch working

--- src/pages/UserPanel/UPFavoritesPage/UPFavoritesPage.tsx
+++ src/pages/UserPanel/UPFavoritesPage/UPFavoritesPage.tsx
@@ -3,9 +3,25 @@ import UPTitle from "../UPTitle/UPTitle";
 import styles from "./UPFavoritesPage.module.css";
 import Footer from "../../../components/Footer/Footer";
 import { useTranslation } from "react-i18next";
+import Card from "../../../components/Card/Card";
+import { getFavorites } from "../../../services/favorite";
+import { useEffect, useState } from "react";
 
 export default function UPFavoritesPage() {
   const { t } = useTranslation();
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+
+    const fetchFavorites = async () => {
+        const response = await getFavorites();
+        const fav = response[0];
+        setFavorites(fav);
+    } 
+
+    fetchFavorites();
+
+  }, [favorites])
 
   return (
     <div style={{ backgroundColor: "var(--primary-color-2)" }}>
@@ -13,7 +29,11 @@ export default function UPFavoritesPage() {
       <div className={styles.page}>
         <UPNav active="5" />
         <div className={styles.content}>
+        {
+          favorites.length > 0 ? 
+            favorites.map((favorite : any) => <Card key={favorite.id} title={favorite.products.name} description={favorite.products.description} price={favorite.products.price}/>) :
           <div style={{ fontWeight: "600" }}>{t("noFavProducts")}</div>
+        }
         </div>
       </div>
       <Footer />
