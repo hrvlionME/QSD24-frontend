7d04abc2a3b6b043ab7e3cfee6f5e8e0c7002edb
Hrvoje Kožul <hrvlionME@gmail.com>
2024-06-08 11:00:20 +0200

    401 infinite loop bug fix

--- src/axios-client.ts
+++ src/axios-client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import store from './redux/store';
 import { RootState } from './redux/store';
 import { refreshAuthToken } from './services/auth';
-import { updateToken } from './redux/userSlice';
+import { updateToken, logout } from './redux/userSlice';
 
 const axiosClient = axios.create({
     baseURL: 'http://localhost:8000/api',
@@ -15,7 +15,7 @@ const getToken = () => {
     const state: RootState = store.getState();
     return state.user.token;
 };
- 
+
 axiosClient.interceptors.request.use(
     (config) => {
         const token = getToken();
@@ -33,24 +33,30 @@ axiosClient.interceptors.response.use(
     (response) => response,
     async (error) => {
         const originalRequest = error.config;
+        const state: RootState = store.getState();
+
         if (error.response && error.response.status === 401 && !originalRequest._retry) {
             originalRequest._retry = true;
+
+            if (!state.user.loggedIn) {
+                store.dispatch(logout());
+                return Promise.reject(error);
+            }
+
             try {
                 const refreshedTokenData = await refreshAuthToken();
                 const { token } = refreshedTokenData.authorization;
                 store.dispatch(updateToken(token));
-                const newToken = getToken();
-                if (newToken) {
-                    originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
-                }
+                originalRequest.headers['Authorization'] = `Bearer ${token}`;
                 return axiosClient(originalRequest);
             } catch (refreshError) {
+                store.dispatch(logout());
                 return Promise.reject(refreshError);
             }
         }
+
         return Promise.reject(error);
     }
 );
-
 
 export default axiosClient;
